a7faa46aabdd5d71f05bb0cb5b36a5a2c97685ff
Mehul Kumar Darji <mehultaylor7@gmail.com>
2023-04-19 23:25:14 +0530

    ADD share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,11 +11,13 @@ import SwiperCore, {
     Pagination
  } from "swiper";
  import "swiper/css/bundle";
+ import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
-    const params = useParams()
-    const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const params = useParams();
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
     SwiperCore.use([Autoplay, Navigation, Pagination]);
     useEffect(() => {
       async function fetchListing() {
@@ -31,13 +33,13 @@ export default function Listing() {
     if (loading) {
       return <Spinner />
     }
-  return 
+  return (
   <main>
     <Swiper slidesPerView={1} navigation pagination={{type: "progressbar"}}
     effect="fade" modules={[EffectFade]} autoplay={{ delay: 3000 }}>
         {listing.imgUrls.map((url,index) => (
           <SwiperSlide key={index}>
-            <div className="w-full overflow-hidden h-[300px]"
+            <div className=" relative w-full overflow-hidden h-[300px]"
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
                 backgroundSize: "cover"
@@ -47,5 +49,17 @@ export default function Listing() {
           </SwiperSlide>
         ))}
     </Swiper>
+  <div className=" fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={()=>{
+    navigator.clipboard.writeText(window.location.href);
+    setShareLinkCopied(true);
+    setTimeout(()=>{
+      setShareLinkCopied(false);
+    }, 2000)
+  }}>
+    <FaShare className=" text-lg text-slate-500"/>
+  </div>
+    {shareLinkCopied && (
+    <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied</p>)}
   </main>
+ );
 }
